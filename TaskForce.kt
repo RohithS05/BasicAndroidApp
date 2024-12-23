7a87d8f6c4d5715800c6150d640336719bb170ca
RohithKumar05 <rohithuoft@gmail.com>
2024-12-23 14:53:13 -0500

    init

--- TaskForce.kt
+++ TaskForce.kt
@@ -1,12 +1,10 @@
 class TaskForce {
-
-}
-
-fun main(){
-    println("talha is smelly")
-    println("robust is smelly mcbumson")
-    val name = "abdul";
-    if (name == "abdul"){
-        println("abdul kill urself")
+    fun main(){
+        println("talha is smelly")
+        println("robust is smelly mcbumson")
+        val name = "abdul";
+        if (name == "abdul"){
+            println("abdul kill urself")
+        }
     }
 }
